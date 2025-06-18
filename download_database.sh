5e24849dfdaccf0e0fa92c3db53ac290e8963c65
ebarbeito <barbeito.enrique@gmail.com>
2025-06-18 10:10:11 -0300

    minor changes

--- download_database.sh
+++ download_database.sh
@@ -2,5 +2,4 @@
 
 rm com.nike.nrc.room.database
 adb pull /data/data/com.nike.plusgps/databases/com.nike.nrc.room.database .
-rm -R gpx
-node nrc2gpx.js
+
---
+++ rootDevice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script tries to remount device as root
+adb root
+
